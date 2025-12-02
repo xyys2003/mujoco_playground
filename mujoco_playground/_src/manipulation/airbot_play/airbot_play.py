1000dbcc70a0a5555e8581192974f43eb595b8ad
TATP-233 <tatp1305116641@gamil.com>
2025-12-02 10:35:26 +0800

    airbot

---
+++ mujoco_playground/_src/manipulation/airbot_play/airbot_play.py
@@ -0,0 +1,93 @@
+from typing import Any, Dict, Optional, Union
+
+from etils import epath
+import jax.numpy as jp
+from ml_collections import config_dict
+import mujoco
+from mujoco import mjx
+import numpy as np
+
+from mujoco_playground._src import mjx_env
+
+_ARM_JOINTS = [
+    "joint1",
+    "joint2",
+    "joint3",
+    "joint4",
+    "joint5",
+    "joint6",
+]
+_FINGER_JOINTS = ["right", "left"]
+
+
+def default_config() -> config_dict.ConfigDict:
+  return config_dict.create(
+      ctrl_dt=0.02,
+      sim_dt=0.005,
+      episode_length=150,
+      action_repeat=1,
+      action_scale=0.04,
+      impl='jax',
+      nconmax=12 * 8192,
+      njmax=44,
+  )
+
+class AirbotPlayBase(mjx_env.MjxEnv):
+  def __init__(
+      self,
+      xml_path: epath.Path,
+      config: config_dict.ConfigDict,
+      config_overrides: Optional[Dict[str, Union[str, int, list[Any]]]] = None,
+  ):
+    super().__init__(config, config_overrides)
+
+    self._xml_path = xml_path.as_posix()
+    mj_model = mujoco.MjModel.from_xml_path(self._xml_path)
+    mj_model.opt.timestep = self.sim_dt
+
+    self._mj_model = mj_model
+    self._mjx_model = mjx.put_model(mj_model, impl=self._config.impl)
+    self._action_scale = config.action_scale
+
+  def _post_init(self, obj_name: str, keyframe: str):
+    all_joints = _ARM_JOINTS + _FINGER_JOINTS
+    self._robot_arm_qposadr = np.array([
+        self._mj_model.jnt_qposadr[self._mj_model.joint(j).id]
+        for j in _ARM_JOINTS
+    ])
+    self._robot_qposadr = np.array([
+        self._mj_model.jnt_qposadr[self._mj_model.joint(j).id]
+        for j in all_joints
+    ])
+    self._gripper_site = self._mj_model.site("endpoint").id
+    self._left_finger_geom = self._mj_model.geom("left_finger_pad").id
+    self._right_finger_geom = self._mj_model.geom("right_finger_pad").id
+    self._obj_body = self._mj_model.body(obj_name).id
+    self._obj_qposadr = self._mj_model.jnt_qposadr[
+        self._mj_model.body(obj_name).jntadr[0]
+    ]
+    # self._mocap_target = self._mj_model.body("mocap_target").mocapid
+    self._floor_geom = self._mj_model.geom("floor").id
+    self._init_q = self._mj_model.keyframe(keyframe).qpos
+    self._init_obj_pos = jp.array(
+        self._init_q[self._obj_qposadr : self._obj_qposadr + 3],
+        dtype=jp.float32,
+    )
+    self._init_ctrl = self._mj_model.keyframe(keyframe).ctrl
+    self._lowers, self._uppers = self._mj_model.actuator_ctrlrange.T
+
+  @property
+  def xml_path(self) -> str:
+    return self._xml_path
+
+  @property
+  def action_size(self) -> int:
+    return self.mjx_model.nu
+
+  @property
+  def mj_model(self) -> mujoco.MjModel:
+    return self._mj_model
+
+  @property
+  def mjx_model(self) -> mjx.Model:
+    return self._mjx_model
---
+++ mujoco_playground/_src/manipulation/airbot_play/pick.py
@@ -0,0 +1,102 @@
+from typing import Any, Dict, Optional, Union
+
+from etils import epath
+import jax
+import jax.numpy as jp
+from mujoco import mjx
+from mujoco_playground._src import mjx_env
+from mujoco_playground._src.manipulation.airbot_play import airbot_play
+from mujoco_playground._src.mjx_env import State
+from ml_collections import config_dict
+
+def default_config() -> config_dict.ConfigDict:
+    """Returns the default config for bring_to_target tasks."""
+    config = config_dict.create(
+        ctrl_dt=0.02,
+        sim_dt=0.005,
+        episode_length=150,
+        action_repeat=1,
+        action_scale=0.04,
+        reward_config=config_dict.create(
+            scales=config_dict.create(
+                # Gripper goes to the box.
+                gripper_box=4.0,
+                # Box goes to the target mocap.
+                box_target=8.0,
+                # Do not collide the gripper with the floor.
+                no_floor_collision=0.25,
+                # Arm stays close to target pose.
+                robot_target_qpos=0.3,
+            )
+        ),
+        impl='jax',
+        nconmax=24 * 2048,
+        njmax=128,
+    )
+    return config
+
+class AirbotPlayPick(airbot_play.AirbotPlayBase):
+    def __init__(
+        self,
+        xml_path: epath.Path,
+        config: config_dict.ConfigDict,
+        config_overrides: Optional[Dict[str, Union[str, int, list[Any]]]] = None,
+    ):
+        super().__init__(xml_path, config, config_overrides)
+        self._post_init(keyframe="home")
+
+    def _post_init(self, keyframe: str):
+        self._gripper_site = self._mj_model.site("endpoint").id
+
+        self._init_q = self._mj_model.keyframe(keyframe).qpos
+        self._init_ctrl = self._mj_model.keyframe(keyframe).ctrl
+        self._lowers, self._uppers = self._mj_model.actuator_ctrlrange.T
+
+    def reset(self, rng: jax.Array) -> State:
+        # rng, rng_box, rng_target = jax.random.split(rng, 3)
+
+        # initialize data
+        init_q = jp.array(self._init_q)
+        data = mjx_env.make_data(
+            self._mj_model,
+            qpos=init_q,
+            qvel=jp.zeros(self._mjx_model.nv, dtype=float),
+            ctrl=self._init_ctrl,
+            impl=self._mjx_model.impl.value,
+            nconmax=self._config.nconmax,
+            njmax=self._config.njmax,
+        )
+
+        # initialize env state and info
+        metrics = {}
+        info = {"rng": rng}
+        obs = self._get_obs(data, info)
+        reward, done = jp.zeros(2, dtype=jp.float32)
+        state = State(data, obs, reward, done, metrics, info)
+        return state
+
+    def _get_obs(self, data: mjx.Data, info: dict[str, Any]) -> jax.Array:
+        gripper_pos = data.site_xpos[self._gripper_site]
+        gripper_mat = data.site_xmat[self._gripper_site].ravel()
+        obs = jp.concatenate([
+            data.qpos,
+            data.qvel,
+            gripper_pos,
+            gripper_mat[3:],
+        ])
+
+        return obs
+
+    def step(self, state: State, action: jax.Array) -> State:
+        delta = action * self._action_scale
+        ctrl = state.data.ctrl + delta
+        ctrl = jp.clip(ctrl, self._lowers, self._uppers)
+
+        data = mjx_env.step(self._mjx_model, state.data, ctrl, self.n_substeps)
+        reward = jp.zeros(1, dtype=jp.float32)
+        done = jp.zeros(1, dtype=jp.float32)
+
+        obs = self._get_obs(data, state.info)
+        state = State(data, obs, reward, done, state.metrics, state.info)
+
+        return state
